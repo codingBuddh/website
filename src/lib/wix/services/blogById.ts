3050f30f920003c1568da089ba551e7da7627f6d
tanmoy <tanmoy@honeyquote.com>
2026-03-31 08:46:42 +0530

    Fix get products and get blogs

--- src/lib/wix/services/blogById.ts
+++ src/lib/wix/services/blogById.ts
@@ -1,7 +1,10 @@
 // lib/getBlogById.ts
+import { unstable_noStore as noStore } from "next/cache";
 import { wixClient } from "../client";
 
 export const getBlogById = async (postId: string) => {
+  noStore();
+
   const post = await wixClient.posts.getPost(postId, {
     fieldsets: ["RICH_CONTENT"] as any,
   } as any);
